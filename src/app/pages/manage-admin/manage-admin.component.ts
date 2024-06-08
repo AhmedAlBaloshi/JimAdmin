ba22b1449d8f80e09e68b2e3faddcebf06cb6b55
AhmedAlbaloshi <ahmed@thiqatech.com>
2024-06-08 21:28:12 +0500

    add 2 agent from 1 form

--- src/app/pages/manage-admin/manage-admin.component.ts
+++ src/app/pages/manage-admin/manage-admin.component.ts
@@ -7,12 +7,15 @@ import { Location } from '@angular/common';
 import * as moment from 'moment';
 import { ApisService } from 'src/app/services/apis.service';
 import { UtilService } from 'src/app/services/util.service';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-manage-admin',
   templateUrl: './manage-admin.component.html',
   styleUrls: ['./manage-admin.component.css'],
 })
 export class ManageAdminComponent implements OnInit {
+  banner_to_upload: any;
+  fileURL: any;
   cities: any[] = [];
   zones: any[] = [];
   new: boolean;
@@ -24,18 +27,25 @@ export class ManageAdminComponent implements OnInit {
   email: any = '';
   password: any = '';
   mobile: any;
+  fname2: any = '';
+  lname2: any = '';
+  email2: any = '';
+  password2: any = '';
+  mobile2: any;
   city: any = '';
   zone_id: any = '';
-  coverImage: any = '';
+  coverImage: any = 'NA';
   status: any = '';
   address: any = '';
   lat: any = '';
   others: any = '';
   lng: any = '';
   gender: any = '1';
+  gender2: any = '1';
 
   imageUrl: any;
   mobileCcode: any = this.api.default_country_code;
+  mobileCcode2: any = this.api.default_country_code;
   constructor(
     private route: ActivatedRoute,
     public api: ApisService,
@@ -87,7 +97,7 @@ export class ManageAdminComponent implements OnInit {
             this.lat = info.lat;
             this.lng = info.lng;
             this.address = info.address;
-            this.getZones()
+            this.getZones();
           } else {
             this.error('Something went wrong');
           }
@@ -132,9 +142,9 @@ export class ManageAdminComponent implements OnInit {
   }
 
   getZones() {
-    this.zones = []
+    this.zones = [];
     this.api
-      .get('zones/getAllZones?city_id='+this.city)
+      .get('zones/getAllZones?city_id=' + this.city)
       .then(
         (data: any) => {
           // console.log(data);
@@ -194,6 +204,10 @@ export class ManageAdminComponent implements OnInit {
   }
 
   create() {
+    if (this.isAgent) {
+      this.createSecondAgent();
+    }
+    console.log(this.coverImage);
     if (
       this.email === '' ||
       this.password === '' ||
@@ -201,7 +215,8 @@ export class ManageAdminComponent implements OnInit {
       this.lname === '' ||
       this.gender === '' ||
       this.mobile === '' ||
-      !this.mobile
+      !this.mobile ||
+      this.coverImage === ''
     ) {
       this.error('All Fields are required');
       return false;
@@ -240,11 +255,11 @@ export class ManageAdminComponent implements OnInit {
       email: this.email,
       password: this.password,
       type: user_type,
-      created_by: this.isAgent?localStorage.getItem('uid'):null,
+      created_by: this.isAgent ? localStorage.getItem('uid') : null,
       status: 1,
       lat: '0',
       lng: '0',
-      cover: 'NA',
+      cover: this.coverImage,
       mobile: this.mobile,
       verified: 1,
       fcm_token: 'NA',
@@ -256,7 +271,7 @@ export class ManageAdminComponent implements OnInit {
       role_id: role_id,
       country_code: '+' + this.mobileCcode,
     };
-console.log('patrama-----------------'+ JSON.stringify(param))
+    console.log('patrama-----------------' + JSON.stringify(param));
     this.spinner.show();
     this.api.post('users/registerUser', param).then((data: any) => {
       console.log(data);
@@ -280,7 +295,8 @@ console.log('patrama-----------------'+ JSON.stringify(param))
       this.lname === '' ||
       this.gender === '' ||
       this.mobile === '' ||
-      !this.mobile
+      !this.mobile ||
+      this.coverImage === ''
     ) {
       this.error('All Fields are required');
       return false;
@@ -313,7 +329,6 @@ console.log('patrama-----------------'+ JSON.stringify(param))
       role_id = 6;
     }
 
-
     const param = {
       full_name: this.fname,
       last_name: this.lname,
@@ -321,6 +336,7 @@ console.log('patrama-----------------'+ JSON.stringify(param))
       lat: this.lat,
       lng: this.lng,
       city: this.city,
+      cover: this.coverImage,
       type: user_type,
       zone_id: this.zone_id,
       mobile: this.mobile,
@@ -343,6 +359,109 @@ console.log('patrama-----------------'+ JSON.stringify(param))
         this.error(data.message);
         return false;
       }
+    });
+  }
+
+  preview_banner(files) {
+    this.banner_to_upload = [];
+    if (files.length === 0) {
+      return;
+    }
+    const mimeType = files[0].type;
+
+    this.banner_to_upload = files;
+    if (this.banner_to_upload) {
+      this.spinner.show();
+      this.api.uploadFile(this.banner_to_upload).subscribe(
+        (data: any) => {
+          this.spinner.hide();
+          if (data && data.status === 200 && data.data) {
+            this.fileURL = data.data;
+            this.coverImage = environment.mediaURL + data.data;
+          }
+        },
+        (err) => {
+          console.log(err);
+          this.spinner.hide();
+        }
+      );
+    } else {
+      console.log('no');
+    }
+  }
+
+  createSecondAgent() {
+    if (this.isAgent) {
+    }
+    console.log(this.coverImage);
+    if (
+      this.email === '' ||
+      this.password === '' ||
+      this.fname === '' ||
+      this.lname === '' ||
+      this.gender === '' ||
+      this.mobile === '' ||
+      !this.mobile ||
+      this.coverImage === ''
+    ) {
+      this.error('All Fields are required');
+      return false;
+    }
+    if (this.isAgent) {
+      if (this.city === '' || this.zone_id === '') {
+        this.error('All Fields are required');
+        return false;
+      }
+    }
+    const emailfilter = /^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailfilter.test(this.email)) {
+      this.error('Please enter valid email');
+      return false;
+    }
+
+    this.spinner.show();
+
+    this.lat = '';
+    this.lng = '';
+    console.log('----->', this.lat, this.lng);
+    let user_type = 'admin';
+    let role_id = 1;
+    if (this.branchManager) {
+      user_type = 'branch_manager';
+      role_id = 5;
+    } else if (this.isAgent) {
+      user_type = 'agent';
+      role_id = 6;
+    }
+
+    const param = {
+      full_name: this.fname2,
+      last_name: this.lname2,
+      gender: 1,
+      email: this.email2,
+      password: this.password2,
+      type: user_type,
+      created_by: this.isAgent ? localStorage.getItem('uid') : null,
+      status: 1,
+      lat: '0',
+      lng: '0',
+      cover: this.coverImage,
+      mobile: this.mobile2,
+      verified: 1,
+      fcm_token: 'NA',
+      others: '1',
+      date: moment().format('YYYY-MM-DD'),
+      stripe_key: '',
+      city: this.isAgent ? this.city : null,
+      zone_id: this.isAgent ? this.zone_id : null,
+      role_id: role_id,
+      country_code: '+' + this.mobileCcode2,
+    };
+    console.log('patrama-----------------' + JSON.stringify(param));
+    this.spinner.show();
+    this.api.post('users/registerUser', param).then((data: any) => {
+      console.log(data);
+      this.spinner.hide();
     });
   }
 }
